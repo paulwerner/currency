49cddc61d2320b8995cd04b3f91ace1367fceb63
Paul Werner <contact@paulwerner.net>
2022-05-20 22:42:57 +0200

    simplify types even more

--- money.go
+++ money.go
@@ -9,23 +9,20 @@ type (
 )
 
 // Number represents a generic type considering int64 or big.Int defined in constants.go
-type Number interface {
+type Amount interface {
 	Int | BigInt
 }
 
-// Amount represents the amount value used for computations
-type Amount Number
-
 // Money holds the monetary unit and currency
-type Money[N Number] struct {
-	amount   N
+type Money[A Amount] struct {
+	amount   A
 	currency *Currency
 }
 
 // New creates a new Money value for the given amount
 // and currency code
-func New[N Number](amount N, code string) *Money[N] {
-	return &Money[N]{
+func New[A Amount](amount A, code string) *Money[A] {
+	return &Money[A]{
 		amount:   amount,
 		currency: &Currency{code},
 	}
