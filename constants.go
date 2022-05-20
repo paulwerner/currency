79076f906e7b71dd88822854508d6456579004fd
Paul Werner <contact@paulwerner.net>
2022-05-20 21:52:26 +0200

    remove currenty irrelevant currencies

--- constants.go
+++ constants.go
@@ -2,50 +2,8 @@ package money
 
 import "math/big"
 
-// constants for currency codes according to ISO 4217
+// constants for  codes according to ISO 4217
 var (
-	// Undefined and testing
-	XXX Currency = Currency{}
-	XTS Currency = Currency{"XTS"}
-
-	// G10 currencies https://en.wikipedia.org/wiki/G10_currencies
-	USD Currency = Currency{"USD"}
-	EUR Currency = Currency{"EUR"}
-	JPY Currency = Currency{"JPY"}
-	GDP Currency = Currency{"GDP"}
-	CHF Currency = Currency{"CHF"}
-	AUD Currency = Currency{"AUD"}
-	NZD Currency = Currency{"NZD"}
-	CAD Currency = Currency{"CAD"}
-	SEK Currency = Currency{"SEK"}
-	NOK Currency = Currency{"NOK"}
-
-	// Additional common currencies
-	BRL Currency = Currency{"BRL"}
-	CNY Currency = Currency{"CNY"}
-	DKK Currency = Currency{"DKK"}
-	INR Currency = Currency{"INR"}
-	RUB Currency = Currency{"RUB"}
-	HKD Currency = Currency{"HKD"}
-	IDR Currency = Currency{"IDR"}
-	KRW Currency = Currency{"KRW"}
-	MXN Currency = Currency{"MXN"}
-	PLN Currency = Currency{"PLN"}
-	SAR Currency = Currency{"SAR"}
-	THB Currency = Currency{"THB"}
-	TRY Currency = Currency{"TRY"}
-	TWD Currency = Currency{"TWD"}
-	ZAR Currency = Currency{"ZAR"}
-
-	// Precious metals
-	XAG Currency = Currency{"XAG"}
-	XAU Currency = Currency{"XAU"}
-	XPT Currency = Currency{"XPT"}
-	XPD Currency = Currency{"XPD"}
+	EUR = "EUR"
 )
 
-// types allowed for monetary unit
-type (
-	Int    = int64
-	BigInt = big.Int
-)
