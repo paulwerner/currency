0021c699a036fa0fc198dd43faefa5bea65d520a
Paul Werner <contact@paulwerner.net>
2022-05-20 22:39:32 +0200

    add Makefile

--- currency.go
+++ currency.go
@@ -1,8 +1,34 @@
 package money
 
+import "strings"
+
 // Currency is an ISO 4217 currency designator
 type Currency struct {
 	Code string
+}
+
+func newCurrency(code string) *Currency {
+	return &Currency{Code: strings.ToUpper(code)}
+}
+
+// GetCurrencyForCode returns the currency for a given code
+// or an money.ErrUnsupportedCurrency error if currency could not be found.
+func GetCurrencyForCode(code string) (*Currency, error) {
+	currency, prs := currencies[code]
+	if !prs {
+		return nil, ErrUnsupportedCurrency
+	}
+	return currency, nil
+}
+
+// MustGetCurrencyForCode returns the currency for a given code
+// or panics if currency could not be found
+func MustGetCurrencyForCode(code string) *Currency {
+	currency, err := GetCurrencyForCode(code)
+	if err != nil {
+		panic(err)
+	}
+	return currency
 }
 
 // Equals returns true, if both Codes are equal,
@@ -14,4 +40,11 @@ func (c *Currency) Equals(oc Currency) bool {
 // String returns the string representation of the currency
 func (c *Currency) String() string {
 	return c.Code
+}
+
+// Currencies holds all the supported currencies for a given code
+type Currencies map[string]*Currency
+
+var currencies = Currencies{
+	EUR: newCurrency("EUR"),
 }
--- money_test.go
+++ money_test.go
@@ -4,15 +4,15 @@ import "testing"
 
 func TestNewGenericInt(t *testing.T) {
 	m := New[Int](1, EUR)
-	if m.amount.val != 1 {
+	if m.amount != 1 {
 		t.Errorf("Expected %d got %d", 1, m.amount)
 	}
-	if m.currency != EUR {
+	if m.currency.Code != EUR {
 		t.Errorf("Expected currency %s got %s", EUR, m.currency)
 	}
 
 	m = New[Int](-100, EUR)
-	if m.amount.val != Int(-100) {
+	if m.amount != Int(-100) {
 		t.Errorf("Expected %d got %d", -100, m.amount)
 	}
 }
