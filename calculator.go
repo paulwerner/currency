ae231411b88ac23052b7b2b3789949d722e9b753
Paul Werner <contact@paulwerner.net>
2022-05-21 18:01:09 +0200

    add calculator

---
+++ calculator.go
@@ -0,0 +1,67 @@
+package money
+
+import "math"
+
+type calculator struct{}
+
+var calc = calculator{}
+
+func (c *calculator) add(a, b Amount) Amount {
+	return a + b
+}
+
+func (c *calculator) sub(a, b Amount) Amount {
+	return a - b
+}
+
+func (c *calculator) multi(a Amount, m int64) Amount {
+	return a * m
+}
+
+func (c *calculator) div(a Amount, d int64) Amount {
+	return a / d
+}
+
+func (c *calculator) mod(a Amount, d int64) Amount {
+	return a % d
+}
+
+func (c *calculator) alloc(a Amount, r, s int) Amount {
+	if r > s {
+		return a
+	}
+	return a * int64(r) / int64(s)
+}
+
+func (c *calculator) abs(a Amount) Amount {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func (c *calculator) neg(a Amount) Amount {
+	if a > 0 {
+		return -a
+	}
+	return a
+}
+
+func (c *calculator) round(a Amount, e int) Amount {
+	if a == 0 {
+		return a
+	}
+	absa := c.abs(a)
+	exp := int64(math.Pow(10, float64(e)))
+	m := c.mod(absa, exp)
+
+	if m > (exp / 2) {
+		absa += exp
+	}
+	absa = (absa / exp) * exp
+	if a < 0 {
+		return -absa
+	} else {
+		return absa
+	}
+}
---
+++ calculator_test.go
@@ -0,0 +1,184 @@
+package money
+
+import "testing"
+
+func TestCalculator_Add(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		b    Amount
+		want Amount
+	}{
+		{1, 2, 3},
+		{-1, 2, 1},
+		{-1, -2, -3},
+	}
+
+	for _, tc := range tcs {
+		if sum := calc.add(tc.a, tc.b); sum != tc.want {
+			t.Errorf("expected %d + %d to be %d, got %d", tc.a, tc.b, tc.want, sum)
+		}
+	}
+}
+
+func TestCalculator_sub(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		b    Amount
+		want Amount
+	}{
+		{1, 2, -1},
+		{-1, 2, -3},
+		{10, -2, 12},
+	}
+
+	for _, tc := range tcs {
+		if diff := calc.sub(tc.a, tc.b); diff != tc.want {
+			t.Errorf("expected %d - %d to be %d, got %d", tc.a, tc.b, tc.want, diff)
+		}
+	}
+}
+
+func TestCalculator_multi(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		b    Amount
+		want Amount
+	}{
+		{1, 2, 2},
+		{-1, 2, -2},
+		{2, -2, -4},
+		{10, -2, -20},
+	}
+
+	for _, tc := range tcs {
+		if prod := calc.multi(tc.a, tc.b); prod != tc.want {
+			t.Errorf("expected %d * %d to be %d, got %d", tc.a, tc.b, tc.want, prod)
+		}
+	}
+}
+
+func TestCalculator_div(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		b    Amount
+		want Amount
+	}{
+		{10, 2, 5},
+		{11, 2, 5},
+		{-10, 2, -5},
+		{-11, 2, -5},
+	}
+
+	for _, tc := range tcs {
+		if quot := calc.div(tc.a, tc.b); quot != tc.want {
+			t.Errorf("expected %d / %d to be %d, got %d", tc.a, tc.b, tc.want, quot)
+		}
+	}
+}
+
+func TestCalculator_mod(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		d    int64
+		want Amount
+	}{
+		{100, 10, 0},
+		{100, 11, 1},
+		{-100, 2, 0},
+		{-100, -2, 0},
+		{-100, -3, -1},
+		{100, 80, 20},
+		{100, 51, 49},
+	}
+
+	for _, tc := range tcs {
+		if mod := calc.mod(tc.a, tc.d); mod != tc.want {
+			t.Errorf("expected %d mod %d to be %d, got %d",
+				tc.a, tc.d, tc.want, mod)
+		}
+	}
+}
+
+func TestCalculator_alloc(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		r    int
+		s    int
+		want Amount
+	}{
+		{100, 10, 50, 20},
+		{100, 2, 50, 4},
+		{100, 2, 500, 0},
+		{100, 80, 500, 16},
+		{100, 51, 50, 100},
+		{100, 2, 1, 100},
+	}
+
+	for _, tc := range tcs {
+		if alloc := calc.alloc(tc.a, tc.r, tc.s); alloc != tc.want {
+			t.Errorf("expected %d allocated in %d ratios with sum %d to be %d, got %d",
+				tc.a, tc.r, tc.s, tc.want, alloc)
+		}
+	}
+}
+
+func TestCalculator_abs(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		want Amount
+	}{
+		{100, 100},
+		{-100, 100},
+	}
+
+	for _, tc := range tcs {
+		if abs := calc.abs(tc.a); abs != tc.want {
+			t.Errorf("expected absolute of %d to be %d, got %d", tc.a, tc.want, abs)
+		}
+	}
+}
+
+func TestCalculator_neg(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		want Amount
+	}{
+		{100, -100},
+		{-100, -100},
+	}
+
+	for _, tc := range tcs {
+		if neg := calc.neg(tc.a); neg != tc.want {
+			t.Errorf("expected negative of %d to be %d, got %d", tc.a, tc.want, neg)
+		}
+	}
+}
+
+func TestCalculator_round(t *testing.T) {
+	calc := calculator{}
+	tcs := []struct {
+		a    Amount
+		e    int
+		want Amount
+	}{
+		{1023, 2, 1000},
+		{1023, 1, 1020},
+		{1, 1, 0},
+		{1, 0, 1},
+	}
+
+	for _, tc := range tcs {
+		if round := calc.round(tc.a, tc.e); round != tc.want {
+			t.Errorf("expected round of %d with %d decimal places to be %d, got %d",
+				tc.a, tc.e, tc.want, round)
+		}
+	}
+}
