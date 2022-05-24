14a24006ffdffc7bf402490819e0d6b9426e6a46
Paul Werner <contact@paulwerner.net>
2022-05-24 02:13:19 +0200

    add common logic

--- pkg/common.go
+++ pkg/common.go
@@ -28,13 +28,24 @@ var roundings = [...]roundingType{
 }
 
 func regionToCode(r Region) uint16 {
-	panic("not implemented")
+	if s := r.String(); len(s) == 2 {
+		return uint16(s[0])<<8 | uint16(s[1])
+	}
+	return 0
 }
 
 func toDate(t time.Time) uint32 {
-	panic("not implemented")
+	y := t.Year()
+	if y == 1 {
+		return 0
+	}
+	date := uint32(y) << 4
+	date |= uint32(t.Month())
+	date <<= 5
+	date |= uint32(t.Day())
+	return date
 }
 
 func fromDate(date uint32) time.Time {
-	panic("not implemented")
+	return time.Date(int(date>>9), time.Month((date>>5)&0xf), int(date&0x1f), 0, 0, 0, 0, time.UTC)
 }
