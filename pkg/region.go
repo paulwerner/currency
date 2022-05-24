4b9fb7eabe07b0ea519effa3543f4bc4982933bd
Paul Werner <contact@paulwerner.net>
2022-05-24 02:13:06 +0200

    add String method to region

--- pkg/region.go
+++ pkg/region.go
@@ -7,3 +7,13 @@ type Region struct {
 	from     string
 	to       string
 }
+
+// String returns the Region's BCP 47 representations
+// it returns "ZZ" for an unspecified region
+func (r Region) String() string {
+	// TODO: fetch from index
+	if r.code == "" {
+		return "ZZ"
+	}
+	return r.code
+}
