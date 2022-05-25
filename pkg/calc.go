6a969ef70820fdeb585ba7cca47b0f03c6e1ee88
Paul Werner <contact@paulwerner.net>
2022-05-25 02:15:30 +0200

    remove value interfaces from calculator

--- pkg/calc.go
+++ pkg/calc.go
@@ -6,53 +6,53 @@ type calculator struct{}
 
 var calc = calculator{}
 
-func (c *calculator) add(a, b Value) Value {
+func (c *calculator) add(a, b int64) int64 {
 	return a + b
 }
 
-func (c *calculator) sub(a, b Value) Value {
+func (c *calculator) sub(a, b int64) int64 {
 	return a - b
 }
 
-func (c *calculator) mul(a Value, m Value) Value {
+func (c *calculator) mul(a Value, m int64) int64 {
 	return a * m
 }
 
-func (c *calculator) div(a Value, d Value) Value {
+func (c *calculator) div(a Value, d int64) int64 {
 	return a / d
 }
 
-func (c *calculator) mod(a Value, d Value) Value {
+func (c *calculator) mod(a int64, d int64) int64 {
 	return a % d
 }
 
-func (c *calculator) alloc(a Value, r, s int) Value {
+func (c *calculator) alloc(a int64, r, s int) int64 {
 	if r > s {
 		return a
 	}
-	return a * Value(r) / Value(s)
+	return a * int64(r) / int64(s)
 }
 
-func (c *calculator) abs(a Value) Value {
+func (c *calculator) abs(a int64) int64 {
 	if a < 0 {
 		return -a
 	}
 	return a
 }
 
-func (c *calculator) neg(a Value) Value {
+func (c *calculator) neg(a int64) int64 {
 	if a > 0 {
 		return -a
 	}
 	return a
 }
 
-func (c *calculator) round(a Value, e int) Value {
+func (c *calculator) round(a int64, e int) int64 {
 	if a == 0 {
 		return a
 	}
 	absa := c.abs(a)
-	exp := Value(math.Pow(10, float64(e)))
+	exp := int64(math.Pow(10, float64(e)))
 	m := c.mod(absa, exp)
 
 	if m > (exp / 2) {
