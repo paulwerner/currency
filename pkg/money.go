5b15f6b23a93b7d15c901e808a4172157b3dc5a4
Paul Werner <contact@paulwerner.net>
2022-05-27 00:07:33 +0200

    add money api

--- pkg/money.go
+++ pkg/money.go
@@ -22,3 +22,114 @@ func NewFromISO(v int64, iso string) (*Money, error) {
 		currency: cur,
 	}, nil
 }
+
+func (m *Money) Currency() *Currency {
+	panic("not implemented")
+}
+
+func (m *Money) Amount() int64 {
+	panic("not implemented")
+}
+
+func (m *Money) SameCurrency(om *Money) bool {
+	panic("not implemented")
+}
+
+func (m *Money) Add(om *Money) (*Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Sub(om *Money) (*Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Mul(mul int64) (*Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Split(d int64) ([]*Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) SplitWithRemainder(d int64) ([]*Money, *Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Alloc(r int64, s *Money) ([]*Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) AllocWithRemainder(r int64, s *Money) ([]*Money, *Money, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Round() *Money {
+	panic("not implemented")
+}
+
+func (m *Money) Display() string {
+	panic("not implemented")
+}
+
+func (m *Money) String() string {
+	panic("not implemented")
+}
+
+func (m *Money) MarshalJSON() ([]byte, error) {
+	panic("not implemented")
+}
+func (m *Money) UnmarshalJSON([]byte) error {
+	panic("not implemented")
+}
+
+func (m *Money) Equals(om *Money) bool {
+	panic("not implemented")
+}
+
+func (m *Money) GreaterThan(om *Money) (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) GreaterThanOrEqual(om *Money) (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) LessThan(om *Money) (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) LessThanOrEqual(om *Money) (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) IsPositive() (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) IsZero() (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) IsNegative() (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Abs() (bool, error) {
+	panic("not implemented")
+}
+
+func (m *Money) Neg() (bool, error) {
+	panic("not implemented")
+}
+
+//
+// PRIVATE
+//
+
+func (m *Money) assertSameCurrency(om *Money) error {
+	panic("not implemented")
+}
+
+func (m *Money) compare(om *Money) int {
+	panic("not implemented")
+}
