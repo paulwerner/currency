8832b09dea85e640f36da1e0cf94954702e3ba55
Paul Werner <contact@paulwerner.net>
2022-05-27 00:50:15 +0200

    add operations to money

--- pkg/money.go
+++ pkg/money.go
@@ -1,5 +1,14 @@
 package money
 
+import "errors"
+
+var (
+	ErrCurrencyMismatch  = errors.New("money: currency mismatch")
+	ErrOperationOverflow = errors.New("money: operation overflow")
+	ErrSplitNegative     = errors.New("money: split must be positive")
+	ErrNoRatioSpecified  = errors.New("money: no ratio specified")
+)
+
 type Money struct {
 	amount   *Amount
 	currency *Currency
@@ -24,43 +33,114 @@ func NewFromISO(v int64, iso string) (*Money, error) {
 }
 
 func (m *Money) Currency() *Currency {
-	panic("not implemented")
+	return m.currency
 }
 
 func (m *Money) Amount() int64 {
-	panic("not implemented")
+	a := int64(m.amount.val)
+	if m.amount.neg {
+		return -a
+	}
+	return a
 }
 
 func (m *Money) SameCurrency(om *Money) bool {
-	panic("not implemented")
+	return m.currency.Equals(om.currency)
 }
 
 func (m *Money) Add(om *Money) (*Money, error) {
-	panic("not implemented")
+	if err := m.assertSameCurrency(om); err != nil {
+		return nil, err
+	}
+
+	z, ok := calc.add(m.amount, om.amount)
+	if !ok {
+		return nil, ErrOperationOverflow
+	}
+	return &Money{
+		amount:   z,
+		currency: m.currency,
+	}, nil
 }
 
 func (m *Money) Sub(om *Money) (*Money, error) {
-	panic("not implemented")
+	if err := m.assertSameCurrency(om); err != nil {
+		return nil, err
+	}
+
+	z, ok := calc.sub(m.amount, om.amount)
+	if !ok {
+		return nil, ErrOperationOverflow
+	}
+	return &Money{
+		amount:   z,
+		currency: m.currency,
+	}, nil
 }
 
 func (m *Money) Mul(mul int64) (*Money, error) {
-	panic("not implemented")
+	z, ok := calc.mul(m.amount, mul)
+	if !ok {
+		return nil, ErrOperationOverflow
+	}
+	return &Money{
+		amount:   z,
+		currency: m.currency,
+	}, nil
 }
 
-func (m *Money) Split(d int64) ([]*Money, error) {
-	panic("not implemented")
+func (m *Money) Split(n int64) ([]*Money, *Money, error) {
+	if n <= 0 {
+		return nil, nil, ErrSplitNegative
+	}
+
+	z, ok := calc.div(m.amount, n)
+	if !ok {
+		return nil, nil, ErrOperationOverflow
+	}
+
+	ms := make([]*Money, n)
+	for i := int64(0); i < n; i++ {
+		ms[i] = &Money{amount: z, currency: m.currency}
+	}
+
+	r := calc.mod(m.amount, n)
+	return ms, &Money{amount: r, currency: m.currency}, nil
 }
 
-func (m *Money) SplitWithRemainder(d int64) ([]*Money, *Money, error) {
-	panic("not implemented")
-}
+func (m *Money) Alloc(rs ...int) ([]*Money, *Money, error) {
+	if len(rs) == 0 {
+		return nil, nil, ErrNoRatioSpecified
+	}
 
-func (m *Money) Alloc(r int64, s *Money) ([]*Money, error) {
-	panic("not implemented")
-}
+	// sum of ratios
+	var sum int
+	for _, r := range rs {
+		sum += r
+	}
 
-func (m *Money) AllocWithRemainder(r int64, s *Money) ([]*Money, *Money, error) {
-	panic("not implemented")
+	var total *Amount
+	var ms []*Money
+
+	for _, r := range rs {
+		a, ok := calc.alloc(m.amount, r, sum)
+		if !ok {
+			return nil, nil, ErrOperationOverflow
+		}
+		party := &Money{
+			amount:   a,
+			currency: m.currency,
+		}
+		ms = append(ms, party)
+		total, ok = calc.add(total, m.amount)
+	}
+
+	// leftover
+	lo, ok := calc.sub(m.amount, total)
+	if !ok {
+		return nil, nil, ErrOperationOverflow
+	}
+	return ms, &Money{amount: lo, currency: m.currency}, nil
 }
 
 func (m *Money) Round() *Money {
