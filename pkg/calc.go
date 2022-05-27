dd72be59649775b19c7456c2d59f5548e77487f6
Paul Werner <contact@paulwerner.net>
2022-05-27 04:02:29 +0200

    add subtraction operatoin

--- pkg/calc.go
+++ pkg/calc.go
@@ -38,7 +38,7 @@ func (c *calculator) add(x, y *Amount) (*Amount, bool) {
 			r.val = val
 		}
 	}
-	r.neg = int64(r.val) > 0 && neg
+	r.neg = neg
 	return &r, true
 }
 
@@ -48,8 +48,37 @@ func _add(x, y value) (sum value, ok bool) {
 	return
 }
 
-func (c *calculator) sub(a, b *Amount) (*Amount, bool) {
-	panic("not implemented")
+func (c *calculator) sub(x, y *Amount) (*Amount, bool) {
+	var r Amount
+	neg := x.neg
+	if x.neg != y.neg {
+		// x - (-y) == x + y
+		// (-x) - y == -(x + y)
+		val, ok := _add(x.val, y.val)
+		if !ok {
+			return nil, false
+		}
+		r.val = val
+	} else {
+		// x - y == x - y == -(y - x)
+		// (-x) - (-y) == y - x == -(x - y)
+		if x.val.cmp(y.val) >= 0 {
+			val, ok := _sub(x.val, y.val)
+			if !ok {
+				return nil, false
+			}
+			r.val = val
+		} else {
+			neg = !neg
+			val, ok := _sub(y.val, x.val)
+			if !ok {
+				return nil, false
+			}
+			r.val = val
+		}
+	}
+	r.neg = neg
+	return &r, true
 }
 
 func _sub(a, b value) (diff value, ok bool) {
--- pkg/calc_test.go
+++ pkg/calc_test.go
@@ -15,7 +15,7 @@ func TestCalc_add(t *testing.T) {
 		t.Errorf("should be ok")
 	}
 	if r.neg {
-		t.Errorf("should positive")
+		t.Errorf("should be positive")
 	}
 	if r.val != 1 {
 		t.Errorf("result should be 1, got: %v", r)
@@ -37,11 +37,56 @@ func TestCalc_addOverflow(t *testing.T) {
 }
 
 func TestCalc_addUnderflow(t *testing.T) {
-	minVal := value(math.MaxUint64)
-	a := &Amount{val: minVal, neg: true}
+	maxVal := value(math.MaxUint64)
+	a := &Amount{val: maxVal, neg: true}
 	b := &Amount{val: 1, neg: true}
 
 	r, ok := calc.add(a, b)
+	if ok {
+		t.Errorf("should not be ok")
+	}
+	if r != nil {
+		t.Errorf("result should be nil")
+	}
+}
+
+func TestCalc_sub(t *testing.T) {
+	maxVal := value(math.MaxUint64)
+	a := &Amount{val: 0, neg: false}
+	b := &Amount{val: maxVal, neg: false}
+
+	r, ok := calc.sub(a, b)
+	if !ok {
+		t.Errorf("should be ok")
+	}
+	if !r.neg {
+		t.Errorf("should be negative")
+	}
+	if r.val != value(math.MaxUint64) {
+		t.Errorf("result should be 1, got: %v", r)
+	}
+}
+
+func TestCalc_subOverflow(t *testing.T) {
+	maxVal := value(math.MaxUint64)
+	a := &Amount{val: maxVal, neg: false}
+	b := &Amount{val: 1, neg: true}
+
+	r, ok := calc.sub(a, b)
+	if ok {
+		t.Errorf("should not be ok")
+	}
+	if r != nil {
+		t.Errorf("result should be nil")
+	}
+}
+
+func TestCalc_subUnderflow(t *testing.T) {
+	maxVal := value(math.MaxUint64)
+	a := &Amount{val: maxVal, neg: true}
+	b := &Amount{val: 1, neg: false}
+
+	r, ok := calc.sub(a, b)
 	if ok {
 		t.Errorf("should not be ok")
 	}
